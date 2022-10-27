1a7206649d9cd7e5b4f29fb2974b28fdb25fc9e9
Aokasa12 <113890892+Aokasa12@users.noreply.github.com>
2022-10-27 23:47:58 +0700

    rererere

--- test/test3.py
+++ test/test3.py
@@ -1,5 +1,7 @@
+from calendar import c
 from tkinter import *
 from tkinter.ttk import Labelframe
+from turtle import right
 from PIL import ImageTk,Image
 
 root = Tk()
@@ -7,20 +9,21 @@ root.title('Python')
 root.geometry("1300x820")
 root.configure(bg = "#F4E8DA")
 
-frame = LabelFrame(root,width = 1024, height = 45,bg = "#DBC2AB").pack()
-frame1 = LabelFrame(root,text = "Student Check",bg = "#DBC2AB")
-frame1.place(x = 5,y =5)
+def logoutclick():
+    print("Hello")
+def plusclick():
+    print("GG")
+    bp = Button(frame1,text = "เพิ่มรายวิชา",bg="#DBC2AB",command = plusclick,height=2,width=170)
 
-b = Button(frame1, text = "Logout")
-b.place(x = 100, y = 5)
 
-frame2 = LabelFrame(root)
-img = ImageTk.PhotoImage(Image.open("Rectangle 4.png"))
+frame = LabelFrame(root,width = 1310, height = 40,bg = "#DBC2AB")
+frame.place(relx =.5, rely = .0,anchor= "n")
+b = Button(frame, text = "Logout",bg = "#DBC2AB",command=logoutclick)
+b.place(x = 1225,y=6)
+a = Label(text = "StudentCheck",bg = "#DBC2AB",font=15).place(x = 10, y = 6)
+frame1 = LabelFrame(root, width=1260,height=740,bg = "#B3947F").place(x = 20, y = 60)
+bp = Button(frame1,text = "เพิ่มรายวิชา",bg="#DBC2AB",command = plusclick,height=2,width=170)
+bp.place(x = 50, y = 100)
 
-label = Label(frame2,image = img,anchor = 'center')
-label.pack()
-
-frame3 = Labelframe(root)
-b2 = Button()
 
 root.mainloop()
